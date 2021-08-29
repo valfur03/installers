c6684d9c23247d1ecbdcf1ed5429f8d9699e2b7b
valfur03 <valentin.furmanek@gmail.com>
2021-08-29 18:34:42 +0200

    Add rootless installations

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+NC="\033[0m"
+BOLD="\033[1m"
+ULINE="\033[4m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+
 # Options
 # 
 # g, gnome -> Gnome
@@ -9,8 +18,7 @@
 print_usage()
 {
 	exec 1>&2
-	printf "Usage: %s [-gkx] distribution\n\n" $(basename "$0")
-	printf "Distributions:\n\t- debian\n"
+	printf "Usage: %s [-gkx]\n" $(basename "$0")
 	exit 1
 }
 
@@ -50,3 +58,44 @@ do
 	echo $DESKTOP
 done
 shift $((OPTIND -1))
+
+distribution=$(grep -E '^(ID_LIKE)=' /etc/os-release | cut -d '=' -f 2)
+if [ -z $distribution ]
+then
+	distribution=$(grep -E '^(ID)=' /etc/os-release | cut -d '=' -f 2)
+	if [ -z $distribution ]
+	then
+		printf "Your ditribution does not seem to be supported...\n"
+		exit 1
+	fi
+fi
+if [ "$distribution" = "debian" ]
+then
+	package_manager="apt-get"
+else
+	printf "Your ditribution does not seem to be supported...\n"
+	exit 1
+fi
+
+# Install oh-my-zsh
+if command -v curl > /dev/null 2>&1
+then
+	yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" "--unattended"
+	if [ $? -ne 0 ]
+	then
+		printf "${RED}oh-my-zsh installation failed, aborting...\n${NC}"
+		exit 1
+	fi
+fi
+
+# Configure VIM
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -o ~/.vimrc https://gist.githubusercontent.com/valfur03/f49e289c6f0b31c24fb167ec8fac461a/raw/.vimrc
+vim +PluginInstall +qall
+mkdir -p ~/.vim/plugin/ftdetect
+echo 'au BufNewFile,BufRead *.c set cindent' >> ~/.vim/plugin/ftdetect/c.vim
+
+# Configure zsh
+curl -o ~/.zshrc https://gist.githubusercontent.com/valfur03/f49e289c6f0b31c24fb167ec8fac461a/raw/.zshrc
+curl -o ~/.zsh_aliases https://gist.githubusercontent.com/valfur03/f49e289c6f0b31c24fb167ec8fac461a/raw/.zsh_aliases
+curl -o ~/.oh-my-zsh/themes/custom.zsh-theme https://gist.githubusercontent.com/valfur03/f49e289c6f0b31c24fb167ec8fac461a/raw/custom.zsh-theme
